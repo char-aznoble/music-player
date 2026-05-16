0c507478981c572dea05137a77a4c67f57554fe3
char-aznoble <chadmcrae1987@gmail.com>
2026-05-16 00:32:21 -0600

    navigation fix

--- app/src/main/java/com/chad/player/MainActivity.kt
+++ app/src/main/java/com/chad/player/MainActivity.kt
@@ -2,13 +2,16 @@ package com.chad.player
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.webkit.JavascriptInterface
 import android.webkit.WebView
+import androidx.activity.OnBackPressedCallback
 import androidx.appcompat.app.AppCompatActivity
 import com.chaquo.python.Python
 import com.chaquo.python.android.AndroidPlatform
 
 class MainActivity : AppCompatActivity() {
     private lateinit var webView: WebView
+    private var canGoBackInternal = false
 
     @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -30,6 +33,27 @@ class MainActivity : AppCompatActivity() {
         webView.settings.allowFileAccess = true
         webView.settings.mediaPlaybackRequiresUserGesture = false
         
+        webView.addJavascriptInterface(WebAppInterface(), "Android")
+
+        onBackPressedDispatcher.addCallback(this, object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                if (canGoBackInternal) {
+                    webView.evaluateJavascript("navigateBack()", null)
+                } else {
+                    isEnabled = false
+                    onBackPressedDispatcher.onBackPressed()
+                    isEnabled = true
+                }
+            }
+        })
+
         webView.loadUrl("file:///android_asset/www/index.html")
+    }
+
+    inner class WebAppInterface {
+        @JavascriptInterface
+        fun setCanGoBack(canGoBack: Boolean) {
+            canGoBackInternal = canGoBack
+        }
     }
 }
